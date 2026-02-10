35da1bbc261433bb3f15a1e918112550db1f8127
Christella <desousa.christella@proton.me>
2026-02-10 14:58:40 +0100

    fix: failure test

--- tests/failure-test.sh
+++ tests/failure-test.sh
@@ -1,11 +1,11 @@
-# #!/bin/bash
+#!/bin/bash
 set -e
 
 HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}" http://localhost:8080/failure)
 
-if [ "$HTTP_STATUS" -eq 200 ]; then
-    echo "Unit test passed: Received HTTP 200"
+if [ "$HTTP_STATUS" -eq 404 ]; then
+    echo "Unit test passed: Received HTTP 404"
 else
-    echo "Unit test failed: Expected HTTP 200 but got $HTTP_STATUS"
+    echo "Unit test failed: Expected HTTP 404 but got $HTTP_STATUS"
     exit 1
 fi
