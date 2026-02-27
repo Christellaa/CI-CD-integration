9165f2c13c687a0a0c31b5f43af41e7a22f18407
Christellaa <99279155+Christellaa@users.noreply.github.com>
2026-02-27 10:15:37 +0100

    Update HTTP endpoint in failure test script

--- tests/failure-test.sh
+++ tests/failure-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}" http://localhost:8080/failure)
+HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}" http://localhost:8081/failure)
 
 if [ "$HTTP_STATUS" -eq 404 ]; then
     echo "Unit test passed: Received HTTP 404"
